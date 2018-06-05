bdd7d0f69478cd9535d17bc9cc1daa61a2483786
Jonathan Kalambay <jonnyk20@gmail.com>
2018-06-05 14:45:39 -0700

    Web Icon Renders Without Error

--- src/App.js
+++ src/App.js
@@ -1,56 +1,13 @@
 import React, { Component } from 'react'
-import { View, Text, StyleSheet } from 'react-native'
-import { Button } from 'react-native-elements';
-import { Ionicons } from '@expo/vector-icons';
+
+import Home from './components/Home'
+import Elements from './components/Elements'
 
 
 export default class App extends Component {
   render() {
     return (
-      <View style={styles.app}>
-        <View style={styles.appHeader}>
-          <Text style={styles.appTitle}>Welcome to React ⚛️</Text>
-        </View>
-        <Text style={styles.appIntro}>
-          Testing my Hybrid App
-        </Text>
-        <View style={styles.container}>
-          <Ionicons name="md-checkmark-circle" size={32} color="green" />
-          <Button
-            raised
-            buttonStyle={{ backgroundColor: '#ff4f00', borderRadius: 10 }}
-            textStyle={{ textAlign: 'center' }}
-            title={`Welcome to\nReact Native Elements`}
-          />
-        </View>
-      </View>
+      <Elements />
     )
   }
 }
-const styles = StyleSheet.create({
-  app: {
-    flex: 1
-  },
-  appHeader: {
-    flex: 1,
-    backgroundColor: '#222',
-    padding: 20,
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  appTitle: {
-    fontSize: 16,
-    color: 'white'
-  },
-  appIntro: {
-    flex: 2,
-    fontSize: 30,
-    textAlign: 'center'
-  },
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#ecf0f1',
-  }
-})
---
+++ src/components/Elements.js
@@ -0,0 +1,98 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, Platform } from 'react-native'
+import { Button } from 'react-native-elements';
+import iconFont from 'react-native-vector-icons/Fonts/FontAwesome.ttf';
+
+let Icon;
+// import Icon from 'react-native-vector-icons/dist/FontAwesome';
+
+// if (Platform.OS === 'android' || Platform.OS === 'ios') {
+//   // import { Ionicons as Icon } from '@expo/vector-icons';
+//   Icon = require('@expo/vector-icons').Ionicons;
+// } else {
+//   // import Icon from 'react-native-vector-icons/dist/FontAwesome';
+//   Icon = require('react-native-vector-icons/dist/FontAwesome');
+// }
+// console.log('Platform.OS', Platform.OS)
+
+let iconFontStyles;
+// Create stylesheet
+if (Platform.OS === 'web') {
+  Icon = require('react-native-vector-icons/dist/FontAwesome').default;
+  console.log('Icon', Icon)
+  iconFontStyles = `@font-face {
+    src: url(${iconFont});
+    font-family: FontAwesome;
+  }`;
+  const style = document.createElement('style');
+  style.type = 'text/css';
+  if (style.styleSheet) {
+    style.styleSheet.cssText = iconFontStyles;
+  } else {
+    style.appendChild(document.createTextNode(iconFontStyles));
+  }
+
+  // Inject stylesheet
+  document.head.appendChild(style);
+}
+
+
+console.log(Platform.OS)
+class Elements extends Component {
+  render() {
+    return (
+      <View style={styles.app}>
+        <View style={styles.appHeader}>
+          <Text style={styles.appTitle}>Welcome to React ⚛️</Text>
+        </View>
+        <Text style={styles.appIntro}>
+          Testing my Hybrid App
+        </Text>
+        <View style={styles.container}>
+          {/* <Icon name="rocket" size={30} color="#900" /> */}
+          {/* <Icon name="home" size={32} color="green" /> */}
+          {
+            Platform.OS === 'web' &&
+            <Icon name="rocket" size={30} color="#900" />
+          }
+          <Button
+            raised
+            buttonStyle={{ backgroundColor: '#ff4f00', borderRadius: 10 }}
+            textStyle={{ textAlign: 'center' }}
+            title={`Welcome to\nReact Native Elements`}
+          />
+        </View>
+      </View>
+    );
+  }
+};
+
+export default Elements;
+
+const styles = StyleSheet.create({
+  app: {
+    flex: 1
+  },
+  appHeader: {
+    flex: 1,
+    backgroundColor: '#222',
+    padding: 20,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  appTitle: {
+    fontSize: 16,
+    color: 'white'
+  },
+  appIntro: {
+    flex: 2,
+    fontSize: 30,
+    textAlign: 'center'
+  },
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#ecf0f1',
+  }
+})
